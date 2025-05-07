6ef66a045c5ec8a8074fe2aa0f7060d9a5bc23a1
oliviermatrot <olivier@matrot.tech>
2025-05-07 18:46:45 +0200

    suppressed a warning

--- HSLocationTracking.cs
+++ HSLocationTracking.cs
@@ -110,7 +110,9 @@ public class HSLocationTracking : IHSLocationManagerDelegate, IDisposable
         }
         else if (status == CLAuthorizationStatus.AuthorizedAlways || status == CLAuthorizationStatus.AuthorizedWhenInUse)
         {
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
             StartLocationTrackingAsync();
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
         }
         else if (status == CLAuthorizationStatus.NotDetermined)
         {
@@ -123,7 +125,9 @@ public class HSLocationTracking : IHSLocationManagerDelegate, IDisposable
         else if (status == CLAuthorizationStatus.Authorized)
         {
             HSLogger.Logger.Write("Location permission granted.");
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
             StartLocationTrackingAsync();
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
         }
         else
         {
